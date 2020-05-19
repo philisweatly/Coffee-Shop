4818b00e2bfcbab7987cfe9d13ca0092ea850376
Phil Smith <philisweatly@gmail.com>
2020-05-19 14:59:33 -0500

    attempting onscroll JS

--- main.js
+++ main.js
@@ -5,3 +5,13 @@ let navBarToggle = document.getElementById('js-navbar-toggle');
 navBarToggle.addEventListener('click', function () {
   mainNav.classList.toggle('active');
 });
+
+let aboutItem = document.getElementsByClassName('about__item');
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 50) {
+    aboutItem.classname = "fadein";
+  } else {
+    aboutItem.classname = "";
+  }
+};
